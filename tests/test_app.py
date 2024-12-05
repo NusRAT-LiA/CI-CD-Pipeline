3e84a34fb8ca067b6f7f571b3a4929af5b9267d9
Nusrat Jahan Lia <nusratlia424@gmail.com>
2024-12-05 13:45:11 +0600

    src location added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from src.app import add
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from app import add
 
 def test_add():
     assert add(1,2) == 3
